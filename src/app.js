fe67157a77d6f42eb45a84019295e923709de4a0
Shaswat Prabhat <spd1238@gmail.com>
2023-08-01 22:54:33 +0530

    Added with in memory local file storage saving of Cat Images

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 const app = express();
-const PORT = 3000; // You can change this to any desired port number
+const PORT = 3000;
 
-// Storage configuration for multer
+const catPictures = [];
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads"); // Destination folder where cat pictures will be stored
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
     const uniqueFileName = uuidv4() + "-" + file.originalname;
@@ -18,50 +20,74 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// app.js
-
-// Upload a cat pic
 app.post("/api/cats", upload.single("catImage"), (req, res) => {
-  // Logic to handle the uploaded cat picture
-  // You can save the file information or do any additional processing here
+  const catId = uuidv4();
 
-  res.status(201).json({ message: "Cat picture uploaded successfully!" });
+  const newCatPicture = {
+    id: catId,
+    originalName: req.file.originalName,
+    filename: req.file.filename,
+  };
+  catPictures.push(newCatPicture);
+
+  res
+    .status(201)
+    .json({ id: catId, message: "Cat picture uploaded successfully!" });
 });
 
-// Delete a cat pic
 app.delete("/api/cats/:id", (req, res) => {
   const catId = req.params.id;
-  // Logic to delete the cat picture with the given ID
-  // Handle the case if the cat picture with the given ID doesn't exist
+
+  const catPictureIndex = catPictures.findIndex((cat) => cat.id === catId);
+
+  if (catPictureIndex === -1) {
+    return res.status(404).json({ error: "Cat picture not found" });
+  }
+
+  const catPicture = catPictures[catPictureIndex];
+
+  const filePath = `./uploads/${catPicture.filename}`;
+  fs.unlinkSync(filePath);
+
+  catPictures.splice(catPictureIndex, 1);
 
   res.json({ message: "Cat picture deleted successfully!" });
 });
 
-// Update a cat pic
 app.put("/api/cats/:id", upload.single("catImage"), (req, res) => {
   const catId = req.params.id;
-  // Logic to update the cat picture with the given ID
-  // Handle the case if the cat picture with the given ID doesn't exist
+
+  const catPictureIndex = catPictures.findIndex((cat) => cat.id === catId);
+
+  if (catPictureIndex === -1) {
+    return res.status(404).json({ error: "Cat picture not found" });
+  }
+
+  const catPicture = catPictures[catPictureIndex];
+
+  const oldFilePath = `./uploads/${catPicture.filename}`;
+  fs.unlinkSync(oldFilePath);
+
+  catPicture.originalname = req.file.originalname;
+  catPicture.filename = req.file.filename;
 
   res.json({ message: "Cat picture updated successfully!" });
 });
 
-// Fetch a particular cat image file by its ID
 app.get("/api/cats/:id", (req, res) => {
   const catId = req.params.id;
-  // Logic to fetch the cat picture with the given ID
-  // Handle the case if the cat picture with the given ID doesn't exist
 
-  // For simplicity, you can just return the file path or URL of the cat picture
-  res.json({ filePath: `/uploads/${catId}-cat.jpg` });
+  const catPicture = catPictures.find((cat) => cat.id === catId);
+
+  if (!catPicture) {
+    return res.status(404).json({ error: "Cat picture not found" });
+  }
+
+  res.json({ filePath: `/uploads/${catPicture.filename}` });
 });
 
-// Fetch a list of the uploaded cat pics
 app.get("/api/cats", (req, res) => {
-  // Logic to fetch a list of all uploaded cat pictures
-  // You can return an array of cat picture information (e.g., file paths or URLs)
-
-  res.json({ catPictures: [] });
+  res.json({ catPictures });
 });
 
 app.listen(PORT, () => {
